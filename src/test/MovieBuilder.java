e3b549107504c8c4076f14a5e8c61b6c05999d19
Marcela Ramirez <marcela.ramirez.ext@adevinta.com>
2023-12-18 21:06:45 +0100

    first commit

---
+++ src/test/MovieBuilder.java
@@ -0,0 +1,28 @@
+package test;
+
+import main.Movie;
+
+class MovieBuilder {
+
+	private String title;
+	private int priceCode;
+
+	MovieBuilder() {
+		title = "movieName";
+	}
+
+	MovieBuilder title(String title) {
+		this.title = title;
+		return this;
+	}
+
+	MovieBuilder priceCode(int priceCode) {
+		this.priceCode = priceCode;
+		return this;
+	}
+
+	Movie build() {
+		return new Movie(title, priceCode);
+	}
+
+}
---
+++ src/test/RentalBuilder.java
@@ -0,0 +1,25 @@
+package test;
+
+import main.Movie;
+import main.Rental;
+
+public class RentalBuilder {
+
+	private Movie movie;
+	private int daysRented;
+
+	public RentalBuilder movie(Movie movie) {
+		this.movie = movie;
+		return this;
+	}
+
+	public RentalBuilder daysRented(int daysRented) {
+		this.daysRented = daysRented;
+		return this;
+	}
+
+	public Rental build() {
+		return new Rental(movie, daysRented);
+	}
+
+}
