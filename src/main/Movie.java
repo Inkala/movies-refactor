81a1efb7ec30d59aa900871164bce9e0f9d1e3c4
MarioRP-01 <marlorosadron@gmail.com>
2023-12-19 22:20:36 +0100

    Remove unused static functions for Movie

--- src/main/Movie.java
+++ src/main/Movie.java
@@ -1,11 +1,6 @@
 package main;
 
 public class Movie {
-
-    public static final int REGULAR = 0;
-    public static final int NEW_RELEASE = 1;
-    public static final int CHILDREN = 2;
-
     private final String title;
     private MovieType movieType;
 
