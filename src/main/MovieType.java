a11844aad9bc7e308b402758979c822ff8e72613
MarioRP-01 <marlorosadron@gmail.com>
2023-12-19 22:17:22 +0100

    Extract extra price magic number to new variable

--- src/main/MovieType.java
+++ src/main/MovieType.java
@@ -8,14 +8,13 @@ public enum MovieType {
         public double getPrice(int daysRented) {
             double price = this.basePrice;
             if (daysRented > 2) {
-                price += (daysRented - 2) * 1.5;
+                price += (daysRented - 2) * this.pricePerExtraDay;
             }
             return price;
         }
     },
     NEW_RELEASE {
-        private final double basePrice = 1.5;
-
+        private final double basePrice = 3;
         @Override
         public double getPrice(int daysRented) {
             return this.basePrice;
@@ -23,17 +22,17 @@ public enum MovieType {
     },
     CHILDREN {
         private final double basePrice = 1.5;
-        private final double pricePerExtraDay = 0;
 
         @Override
         public double getPrice(int daysRented) {
             double price = this.basePrice;
             if (daysRented > 3) {
-                price += (daysRented - 1) * 1.5;
+                price += (daysRented - 1) * this.pricePerExtraDay;
             }
             return 0;
         }
     };
 
+    protected final double pricePerExtraDay = 1.5;
     public abstract double getPrice(int daysRented);
 }
